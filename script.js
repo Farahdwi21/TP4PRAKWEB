4fa533c1e96e495a4ad6d253debd8fae7e519557
Farahdwi21 <99456071+Farahdwi21@users.noreply.github.com>
2022-03-29 23:23:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function submitdata(){
+	var nama = document.getElementById("name").value;
+	var lahir = document.getElementById("birth").value;
+	var gender = document.getElementById("jenis_kelamin").value;
+	var hobi = document.getElementById("hobby").value;
+}
